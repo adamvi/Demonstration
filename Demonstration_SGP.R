156fbfe8eb59dc79164d024b9d0bee19b91647e7
dbetebenner <dbetebenner@gmail.com>
2013-07-01 15:47:24 -0400

    Update of Demonstration_SGP

--- Demonstration_SGP.R
+++ Demonstration_SGP.R
@@ -13,15 +13,14 @@ options(error=recover)
 ### abcSGP
 
 Demonstration_SGP <- abcSGP(
-			sgp_object=sgpData_LONG,
-			data_supplementary=list(INSTRUCTOR_NUMBER=sgpData_INSTRUCTOR_NUMBER),
-			sgPlot.demo.report=TRUE,
-			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SUMMARY=4, GA_PLOTS=4, SG_PLOTS=1)))
+	sgp_object=sgpData_LONG,
+	data_supplementary=list(INSTRUCTOR_NUMBER=sgpData_INSTRUCTOR_NUMBER),
+	sgPlot.demo.report=TRUE,
+	parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SUMMARY=4, GA_PLOTS=4, SG_PLOTS=1)))
 
 ### Output SchoolView tables
 
 outputSGP(Demonstration_SGP, output.type="SchoolView")
-
 
 ### Save results
 
