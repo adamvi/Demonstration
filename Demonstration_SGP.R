d355fb8b3555336eb04a8e2fb229f10886cced7c
dbetebenner <dbetebenner@gmail.com>
2013-12-23 13:11:00 -0500

    Update of goodness of fit plots to include SIMEX adjustment

--- Demonstration_SGP.R
+++ Demonstration_SGP.R
@@ -7,21 +7,34 @@
 ### Load SGP Package
 
 require(SGP)
+require(SGPdata)
 options(error=recover)
+
+
+### Load SIMEX matrices
+
+load("Data/Base_Files/DEMO_SIMEX_Baseline_Matrices.Rdata")
+
+
+### prepareSGP
+
+Demonstration_SGP <- prepareSGP(sgpData_LONG, data_supplementary=list(INSTRUCTOR_NUMBER=sgpData_INSTRUCTOR_NUMBER))
+Demonstration_SGP@SGP$Coefficient_Matrices <- DEMO_SIMEX_Baseline_Matrices
 
 
 ### abcSGP
 
 Demonstration_SGP <- abcSGP(
-	sgp_object=sgpData_LONG,
-	data_supplementary=list(INSTRUCTOR_NUMBER=sgpData_INSTRUCTOR_NUMBER),
-	sgp.target.scale.scores=TRUE,
-	sgPlot.demo.report=TRUE,
+	sgp_object=Demonstration_SGP,
+	steps=c("analyzeSGP", "combineSGP", "summarizeSGP", "outputSGP"),
+	calculate.simex.baseline=TRUE,
 	parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4, SUMMARY=4, GA_PLOTS=4, SG_PLOTS=1)))
+
 
 ### Output SchoolView tables
 
 outputSGP(Demonstration_SGP, output.type="SchoolView")
+
 
 ### Save results
 
